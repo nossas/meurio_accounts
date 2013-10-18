56522820dc9a87caacc9a14d8ec2d9f3544e8e60
Nícolas Iensen <nicolas@iensen.me>
2013-10-18 14:04:22 -0300

    Configure session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
