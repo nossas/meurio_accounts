9c36426b1c7a3cd639cec492dd6efb8b07222232
Nícolas Iensen <nicolas@iensen.me>
2013-10-18 13:44:54 -0300

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeurioAccounts::Application.config.secret_key_base = 'c9a9f838489ee41537e443c03bd7a391e3af30ddc4280452574c5de3c33e87f174473accce3b1c7dc5edf70c8b90c602ffe47b9fe626e57533dc682d469b0855'
