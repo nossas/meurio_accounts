88f149ae4c03d254fc2d8e7026c5019b964491f9
Josias Schneider <josias.ds@gmail.com>
2013-10-31 15:08:39 -0200

    [#1164163] add first and last name to user; add user model specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter { session[:redirect_url] ||= params[:redirect_url] }
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action { session[:redirect_url] ||= params[:redirect_url] }
 
   def after_sign_in_path_for(resource)
     session.delete(:redirect_url) || "#{ENV["MR_USER_PATH"]}/#{current_user.id}"
+  end
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true
+  validates :email, format: { with: /([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}/ }
+  validates :password, on: :create, length: { minimum: 6, maximum: 128 }
 end
