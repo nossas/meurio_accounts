ef27878cc9ddb487f6329b686e420ba78672fa2c
Josias Schneider <josias.ds@gmail.com>
2013-11-01 16:09:36 -0200

    [#1167066] add logout link; add login route

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131101142841) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
