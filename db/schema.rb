2eb399710832ad89a74c128dd2cb86d3df5a01f7
Nícolas Iensen <nicolas@iensen.me>
2013-11-22 08:54:21 -0200

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131121165448) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
