5238b1563b28cc1128c2ffe2555198be980fc44c
Nícolas Iensen <nicolas@iensen.me>
2013-12-03 13:57:19 -0200

    [#61322926] add logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < InheritedResources::Base
     redirect_to session.delete(:redirect_url)
   end
 
+  def logout
+    sign_out current_user
+    redirect_to session.delete(:redirect_url) || root_path
+  end
+
   def permitted_params
     {:user => params.require(:user).permit(:avatar, :first_name, :last_name, :email, :bio, :birthday, :profession, :postal_code, :phone, :secondary_email, :gender, :public, :facebook, :twitter, :website, :availability, :skills, :topics)}
   end
