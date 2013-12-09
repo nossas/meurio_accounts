009e1a73e2c81fd5713eaf38715192158236ee8a
Josias Schneider <josias.ds@gmail.com>
2013-12-09 16:44:27 -0200

    [#1207875] change reset password email

--- app/mailers/custom_mailer.rb
+++ app/mailers/custom_mailer.rb
@@ -1,7 +1,7 @@
 class CustomMailer < Devise::Mailer
 
 	def reset_password_instructions(record, token, opts={})
-		opts[:from] = 'fernanda@meurio.org.br'
+		opts[:from] = 'Meu Rio <fernanda@meurio.org.br>'
   	super
   end
 
