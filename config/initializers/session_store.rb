c2e5c4e46fc5d5fd6cc38aad0d57da92fbc288f9
Josias Schneider <josias.ds@gmail.com>
2014-02-14 18:19:26 -0200

    add expire_after option to session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
 elsif Rails.env.staging?
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br'
+  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
 else
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
 end
