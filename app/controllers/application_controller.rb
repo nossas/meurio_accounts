63a6ba24f9514578e2b9d6a2adca38767da2b86e
Josias Schneider <josias.ds@gmail.com>
2014-02-17 14:06:13 -0300

    Debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  prepend_before_action { session[:redirect_url] ||= params[:redirect_url] }
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action { session[:redirect_url] ||= params[:redirect_url] }
   before_action { session.delete(:flash) }
 
   rescue_from CanCan::AccessDenied do |exception|
