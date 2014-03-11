d15bf0d413b0a228644317a61ed5f2a09acefaaa
Josias Schneider <josias.ds@gmail.com>
2014-03-11 18:16:53 -0300

    [#66369758] add year and month select to datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,7 +33,9 @@ $(function(){
     monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
     monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
     nextText: 'Próximo',
-    prevText: 'Anterior'
+    prevText: 'Anterior',
+    changeMonth: true,
+    changeYear: true
   });
 
   $('form.edit_user a.btn').click(function(){ $('input#user_avatar').trigger('click'); });
