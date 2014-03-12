706f482875936943c72a38cbcff59f25f7895a3d
Josias Schneider <josias.ds@gmail.com>
2014-03-12 19:10:18 -0300

    [#67307740] add Mailchimp stubs for testing

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,1 +1,7 @@
+require 'webmock/cucumber'
 WebMock.disable_net_connect!(:allow_localhost => true)
+
+Before do
+  stub_request(:post, "https://us6.api.mailchimp.com/2.0/stub_chain/and-return")
+  stub_request(:post, "https://us6.api.mailchimp.com/2.0/lists/subscribe")
+end
