781fb5bb58c95327b79ce778645afe215c891852
Nícolas Iensen <nicolas@iensen.me>
2014-05-09 12:18:32 -0300

    [#70847594] debug casino

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.production?
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
-elsif Rails.env.staging?
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
-else
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
-end
+# if Rails.env.production?
+#   MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
+# elsif Rails.env.staging?
+#   MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
+# else
+#   MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
+# end
+
+MeurioAccounts::Application.config.session_store :cookie_store, key: '_MeurioAccounts_session'
