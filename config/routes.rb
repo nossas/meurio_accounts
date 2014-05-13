e30adbbe70261de0cb1d33c26309e3361edbb380
Josias Schneider <josias.ds@gmail.com>
2014-05-13 11:38:31 -0300

    fix layout of casino/login; translate casino messages

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ MeurioAccounts::Application.routes.draw do
   get 'ssi_redirect' => 'users#ssi_redirect', as: :ssi_redirect
   get 'edit_profile' => 'users#edit', as: :edit_profile
 
-  get 'validate_email' => "users#validate_email"
-  post 'validate_email' => "users#validate_email"
+  match 'validate_email' => "users#validate_email", as: :validate_email, via: [:get, :post]
 end
