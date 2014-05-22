138b11b710929a1fb1fbbe4397be1756f8d7f2ad
Nícolas Iensen <nicolas@iensen.me>
2014-05-22 08:23:00 -0300

    [#71766110] add some validations in the organization model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Organization do
+  before { Organization.make! }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :mailchimp_list_id }
+  it { should validate_uniqueness_of :name }
+  it { should validate_uniqueness_of :city }
+  it { should validate_uniqueness_of :mailchimp_list_id }
 end
