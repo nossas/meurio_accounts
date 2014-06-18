7ce87bf10571346a7d551fc7543ec7dcdcad0cc4
Nícolas Iensen <nicolas@iensen.me>
2014-06-18 11:53:49 -0300

    Fix I18n for dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,6 @@ MeurioAccounts::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   default_url_options[:host] = "localhost:3000"
+
+  config.i18n.fallbacks = true
 end
