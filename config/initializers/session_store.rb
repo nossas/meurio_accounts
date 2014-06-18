82d5ed28bfad469529428412006fb43231900ffe
Nícolas Iensen <nicolas@iensen.me>
2014-06-18 11:25:42 -0300

    Remove session store of the old login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.production?
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'minhascidades.org.br', expire_after: 100.years
-elsif Rails.env.staging?
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
-else
-  MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
-end
+MeurioAccounts::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
