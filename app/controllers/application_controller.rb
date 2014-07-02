61ebb12c82ef2c36de5461016d02394f0c94a509
Nícolas Iensen <nicolas@iensen.me>
2014-07-02 14:47:50 -0300

    Debug flash alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to '/login', flash: {alert: exception.message}
+    redirect_to '/login', alert: exception.message
   end
 
   # def after_sign_in_path_for(resource)
