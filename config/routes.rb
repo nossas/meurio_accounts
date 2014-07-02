0bccabf3e64d56c640d8c6df7d05cc82832aa15c
Nícolas Iensen <nicolas@iensen.me>
2014-07-02 15:33:08 -0300

    Debug routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ MeurioAccounts::Application.routes.draw do
   resources :users, only: [:edit, :create, :update, :show]
 
   get 'ssi_redirect' => 'users#ssi_redirect', as: :ssi_redirect
-  get 'profile' => 'users#edit', as: :edit_profile
+  get 'edit_profile' => 'users#edit', as: :edit_profile
   get 'sign_in_after_sign_up' => 'users#sign_in', as: :sign_in_after_sign_up
 
   match 'validate_email' => "users#validate_email", as: :validate_email, via: [:get, :post]
