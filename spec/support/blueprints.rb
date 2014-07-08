48a3a96afa3964092e55ca4a735484f1595f283f
Nícolas Iensen <nicolas@iensen.me>
2014-07-08 15:20:42 -0300

    [#74520630] fix tests

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,10 +1,11 @@
 require 'machinist/active_record'
 
 User.blueprint do
-  first_name    { Faker::Name.first_name }
-  last_name     { Faker::Name.last_name }
-  email         { Faker::Internet.email }
-  password      { Faker::Internet.password }
+  first_name      { Faker::Name.first_name }
+  last_name       { Faker::Name.last_name }
+  email           { Faker::Internet.email }
+  password        { Faker::Internet.password }
+  mailchimp_euid  { sn }
 end
 
 Organization.blueprint do
