d3f48e9553bf37b94cd55cfc6b5290246c34b578
Nícolas Iensen <nicolas@iensen.me>
2014-07-09 16:34:52 -0300

    [#74520224] add membership controller

---
+++ spec/controllers/memberships_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe MembershipsController do
+  describe 'POST create' do
+    before { @user = User.make! }
+    before { @organization = Organization.make! }
+
+    context 'when an organization_id is passed as parameter' do
+      it 'should create a new membership' do
+        expect {
+          post(
+            :create,
+            user_id: @user.id,
+            membership: {organization_id: @organization.id},
+            format: :json,
+            token: ENV["API_TOKEN"]
+          )
+        }.to change{ Membership.count }.by(1)
+      end
+    end
+
+    context 'when no organization_id is passed as parameter' do
+      it 'should not create a new membership' do
+        expect {
+          post(
+            :create,
+            user_id: @user.id,
+            membership: {user_id: @user.id},
+            format: :json,
+            token: ENV["API_TOKEN"]
+          )
+        }.to_not change{ Membership.count }.by(1)
+      end
+    end
+  end
+end
