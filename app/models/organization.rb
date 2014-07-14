8147a67e172cc2d40d2b5dd7cffd02747a16122a
Josias Schneider <josias.ds@gmail.com>
2014-07-14 15:43:36 -0300

    link Organization with User

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,5 +2,8 @@ class Organization < ActiveRecord::Base
   validates :name, :mailchimp_list_id, :city, presence: true
   validates :name, :mailchimp_list_id, :city, uniqueness: true
 
+  has_many :memberships
+  has_many :users, through: :memberships
+
   mount_uploader :avatar, AvatarUploader
 end
