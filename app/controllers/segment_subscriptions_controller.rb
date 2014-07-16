d8adc0e2bce14fb9efa23e4ecf9e12df70e8facd
Nícolas Iensen <nicolas@iensen.me>
2014-07-16 15:22:18 -0300

    [#74981548] change segment id argument for the Accounts API

--- app/controllers/segment_subscriptions_controller.rb
+++ app/controllers/segment_subscriptions_controller.rb
@@ -8,7 +8,9 @@ class SegmentSubscriptionsController < InheritedResources::Base
       if organization = Organization.find(params[:segment_subscription][:organization_id])
         Gibbon::API.lists.static_segment_members_add(
           id: organization.mailchimp_list_id,
-          seg_id: organization.multitude_mailchimp_segment_id,
+          # TODO: after create the Segments table we must change segment_id to represent
+          # Segment#id instead of Segment#mailchimp_uid
+          seg_id: params[:segment_subscription][:segment_id],
           batch: [{ email: User.find(params[:user_id]).try(:email) }]
         )
       end
