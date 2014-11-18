666f39720b4382233b42e3c194dcda95ef224fa9
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-18 09:08:08 -0200

    [#82680396] change user#update_mailchimp_subscription to work with just one Mailchimp list

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,30 +40,28 @@ class User < ActiveRecord::Base
   end
 
   def update_mailchimp_subscription
-    self.organizations.each do |organization|
-      begin
-        subscription = Gibbon::API.lists.subscribe(
-          id: organization.mailchimp_list_id,
-          email: { email: self.email },
-          merge_vars: {
-            FNAME: self.first_name,
-            LNAME: self.last_name,
-            CITY: self.city,
-            PHONE: self.phone,
-            LOGINLINK: self.login_url,
-            DISTRICT: self.address_district,
-            groupings: [ name: 'Skills', groups: self.translated_skills ]
-          },
-          double_optin: false,
-          update_existing: true,
-          replace_interests: true
-        )
+    begin
+      subscription = Gibbon::API.lists.subscribe(
+        id: ENV["MAILCHIMP_LIST_ID"],
+        email: { email: self.email },
+        merge_vars: {
+          FNAME: self.first_name,
+          LNAME: self.last_name,
+          CITY: self.city,
+          PHONE: self.phone,
+          LOGINLINK: self.login_url,
+          DISTRICT: self.address_district,
+          groupings: [ name: 'Skills', groups: self.translated_skills ]
+        },
+        double_optin: false,
+        update_existing: true,
+        replace_interests: true
+      )
 
-        self.update_column :mailchimp_euid, subscription["euid"]
-      rescue Exception => e
-        Appsignal.add_exception e
-        Rails.logger.error e
-      end
+      self.update_column :mailchimp_euid, subscription["euid"]
+    rescue Exception => e
+      Appsignal.add_exception e
+      Rails.logger.error e
     end
   end
 
