b47c82aa4eb7c623cffb00a65bcf4c4b61816aa6
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-21 16:56:01 -0200

    Fix migration

--- db/migrate/20141118131155_remove_mailchimp_list_id_from_organization.rb
+++ db/migrate/20141118131155_remove_mailchimp_list_id_from_organization.rb
@@ -1,5 +1,5 @@
 class RemoveMailchimpListIdFromOrganization < ActiveRecord::Migration
   def change
-    remove_column :organizations, :mailchimp_list_id, :integer, foreign_key: false
+    remove_column :organizations, :mailchimp_list_id, :string, foreign_key: false
   end
 end
