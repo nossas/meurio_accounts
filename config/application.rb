755c2fb6db8ec95b6f52bd33b63e4b3ed2bf0691
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-16 13:33:31 -0300

    Fix i18n for Devise strings

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module MeurioAccounts
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     config.i18n.available_locales = [:pt, :en]
     config.i18n.default_locale = :pt
 
