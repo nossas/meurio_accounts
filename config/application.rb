4ff25c38555d2b88d7c5c47e92dfbb2243d5650b
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-16 15:56:29 -0300

    [#92568214] changed default language to pt-BR

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,8 @@ module MeurioAccounts
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
-    config.i18n.available_locales = [:pt, :en]
-    config.i18n.default_locale = :pt
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = "pt-BR"
 
     config.sass.preferred_syntax = :sass
     config.active_record.schema_format = :sql
