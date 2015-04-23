d58eb8da0f78d8c7089df5c0db58b06eadd1bad7
Josias Schneider <josias.ds@gmail.com>
2015-04-23 16:28:04 -0300

    [#91970780] fix specs

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -2,40 +2,18 @@ require 'spec_helper'
 
 describe Membership do
   it { should belong_to :organization }
-  it { should validate_uniqueness_of(:organization_id).scoped_to(:user_id) }
   it { should validate_presence_of(:organization_id) }
   it { should validate_presence_of(:user) }
 
-  # describe '#after_create' do
-  #   it 'should subscribe user to the mailchimp list' do
-  #     membership = Membership.new
-  #     lists = double('lists')
-  #     allow(Gibbon::API).to receive(:lists).and_return(lists)
-  #     lists.should_receive(:subscribe).with(
-  #       id: membership.organization.mailchimp_list_id,
-  #       email: {email: membership.user.email},
-  #       merge_vars: {
-  #         FNAME: membership.user.first_name,
-  #         LNAME: membership.user.last_name,
-  #         CITY: membership.user.city,
-  #         PHONE: membership.user.phone,
-  #         groupings: [
-  #           { name: 'Skills', groups: membership.user.translated_skills },
-  #           { name: 'Organizations', groups: self.user.organization_names }
-  #         ]
-  #       },
-  #       double_optin: false,
-  #       update_existing: true,
-  #       replace_interests: true
-  #     ).at_least(:once).and_return({"euid" => "123"})
-  #     membership.save!
-  #   end
+  describe "#after_create" do
+    before do
+      allow_any_instance_of(User).to receive(:update_location_and_mailchimp).and_return(true)
+      @user = User.make!
+    end
 
-  #   it 'should update the user mailchimp_euid' do
-  #     user = User.make!
-  #     Gibbon::API.stub_chain(:lists, :subscribe).and_return({"euid" => "123"})
-  #     user.should_receive(:update_attribute).with(:mailchimp_euid, '123')
-  #     membership = Membership.make! user: user
-  #   end
-  # end
+    it 'calls update method from user' do
+      expect(@user).to receive(:update_location_and_mailchimp).once
+      Membership.make! user: @user
+    end
+  end
 end
