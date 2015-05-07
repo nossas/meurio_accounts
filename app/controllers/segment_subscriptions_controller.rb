44f8471d8595d608ed94ae4fc4803438b58c0891
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-07 11:11:04 -0300

    [#91973974] changed segment subscriptions controller to create subscritpion before add to segment

--- app/controllers/segment_subscriptions_controller.rb
+++ app/controllers/segment_subscriptions_controller.rb
@@ -5,15 +5,16 @@ class SegmentSubscriptionsController < InheritedResources::Base
 
   def create
     begin
-      if organization = Organization.find(params[:segment_subscription][:organization_id])
-        Gibbon::API.lists.static_segment_members_add(
-          id: organization.mailchimp_list_id,
-          # TODO: after create the Segments table we must change segment_id to represent
-          # Segment#id instead of Segment#mailchimp_uid
-          seg_id: params[:segment_subscription][:segment_id],
-          batch: [{ email: User.find(params[:user_id]).try(:email) }]
-        )
-      end
+      user = User.find(params[:user_id])
+      user.update_mailchimp_subscription
+
+      Gibbon::API.lists.static_segment_members_add(
+        id: ENV["MAILCHIMP_LIST_ID"],
+        # TODO: after create the Segments table we must change segment_id to represent
+        # Segment#id instead of Segment#mailchimp_uid
+        seg_id: params[:segment_subscription][:segment_id],
+        batch: [{ email: .try(:email) }]
+      )
     rescue Exception => e
       Appsignal.add_exception e
       Rails.logger.error e
