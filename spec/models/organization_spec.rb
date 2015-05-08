56941db72c185a81f3c6700338496d445570aef7
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-08 12:41:07 -0300

    [#91974076] fixed tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,7 +4,6 @@ describe Organization do
   before { Organization.make! }
   it { should validate_presence_of :name }
   it { should validate_presence_of :city }
-  it { should validate_presence_of :mailchimp_list_id }
   it { should validate_uniqueness_of :name }
   it { should validate_uniqueness_of :city }
 end
