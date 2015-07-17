a42b595f6161f18b2d6c44185bc6ef04df05c00f
Josias Schneider <josias.ds@gmail.com>
2015-07-17 12:07:03 -0300

    [#97448438] bug fix: set organizations variable on registrations#new and #edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
-  def new
-    @organizations = Organization.order(:city)
-    super
-  end
+  before_action :set_organizations, only: [:new, :create]
 
   def after_sign_up_path_for(resource)
     sign_in_after_sign_up_path(params: params)
+  end
+
+  private
+  def set_organizations
+    @organizations = Organization.order(:city)
   end
 end
